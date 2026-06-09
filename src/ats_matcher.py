a95a421783004984f89f8a7ba428276eaaeee9d7
Paramesh05-svg <vparamesh1999@gmail.com>
2026-06-09 15:46:19 +0530

    Add calculate_match function for skill comparison
    Implement skill matching logic and calculate score.

--- src/ats_matcher.py
+++ src/ats_matcher.py
@@ -1,2 +1,45 @@
-def calculate_match(candidate_skills, job_skills):
-    return 0
+def calculate_match(
+        candidate_skills,
+        job_skills):
+
+    matched = set(
+        candidate_skills
+    ).intersection(
+        set(job_skills)
+    )
+
+    if len(job_skills) == 0:
+        return 0, []
+
+    score = int(
+        len(matched)
+        / len(job_skills)
+        * 100
+    )
+
+    return score, list(matched)
+
+
+if __name__ == "__main__":
+
+    candidate = [
+        "AWS",
+        "Linux",
+        "Terraform",
+        "Python"
+    ]
+
+    job = [
+        "AWS",
+        "Linux",
+        "Docker",
+        "Terraform"
+    ]
+
+    score, matched = calculate_match(
+        candidate,
+        job
+    )
+
+    print(score)
+    print(matched)
