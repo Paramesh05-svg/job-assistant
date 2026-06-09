4d4036e3a58400ef0bd3882161b9c0444292cec1
Paramesh05-svg <vparamesh1999@gmail.com>
2026-06-09 22:20:06 +0530

    Update config_loader.py

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -1,5 +1,12 @@
 import yaml
 
+
 def load_keywords(path="config/keywords.yaml"):
-    with open(path, "r", encoding="utf-8") as file:
+
+    with open(
+        path,
+        "r",
+        encoding="utf-8"
+    ) as file:
+
         return yaml.safe_load(file)
